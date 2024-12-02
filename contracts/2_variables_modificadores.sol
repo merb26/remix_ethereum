e25c9ebb43153ff864519a2c7edf35cef40a4a5a
merb26 <manuele.ramirez.26@gmail.com>
2024-12-02 11:31:34 -0600

    tipo de datos y modificadores 2

--- contracts/2_variables_modificadores.sol
+++ contracts/2_variables_modificadores.sol
@@ -15,5 +15,36 @@ contract variables_modifiers {
     string private str;
     string public letras = "esto es publico";
 
+    // Booleano
+    bool public booleano = true;
+
+    // Variables bytes
+    bytes32 byte1;
+    bytes4 byte2;
+    bytes1 byte3;
+
+    // Algoritmo de hash
+    bytes32 public hashing = keccak256(abi.encodePacked("Manolo"));
+
+    // Variable address
+    address mi_direccion;
+    address public direccion1 = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
+    address public direccion2 = msg.sender;
+
+    // Enumeracion
+    enum options {ON, OFF}
+    options state;
+    options constant defaultChoice = options.OFF;
     
+    function turnOn() public {
+        state = options.ON;
+    }
+
+    function turnOff() public {
+        state = options.OFF;
+    }
+
+    function displayState() public view returns (options) {
+        return state;
+    }
 }
