302c180d4070847eee71a8299d49730c2835b68c
merb26 <manuele.ramirez.26@gmail.com>
2024-12-02 09:57:07 -0600

    tipo de datos y modificadores

---
+++ contracts/2_variables_modificadores.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract variables_modifiers {
+
+    // Valores enteros sin signo (uint)
+    uint entero;
+    uint public entB = 5;
+
+    // Valores enteros con signo
+    int a;
+    int8 public g = 45;
+
+    // Cadena de caracteres ( String )
+    string private str;
+    string public letras = "esto es publico";
+
+    
+}
